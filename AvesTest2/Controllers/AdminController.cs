692195c724b77077e42c8b509a9b433567e6e33b
Raul <rrchirinosc@gmail.com>
2020-03-13 01:49:35 +0100

    Improved AddingBird function, made bird families available in a combobox for adding new birds to DB

--- AvesTest2/Controllers/AdminController.cs
+++ AvesTest2/Controllers/AdminController.cs
@@ -4,33 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using AvesTest2.Database.DTO;
 using AvesTest2.Database.Repositories;
+using AvesTest2.Models;
 using Microsoft.AspNetCore.Mvc;
+
+//TODO: Implement caching
 
 namespace AvesTest2.Controllers
 {
     public class AdminController : Controller
     {
-        private BirdsRepository repo;
+        private AdminViewModel model;
 
         public AdminController()
         {
-            repo = new BirdsRepository();
         }
 
-        public IActionResult Admin()
+        public async Task<IActionResult> Admin()
         {
-     
-            return View();
+            model = await AdminViewModel.Load();
+            return View(model);
         }
 
         public async Task<IActionResult> AddBird(string Name, string SciName, int FamilyId)
         {
             BirdDTO bird = new BirdDTO();
-            bird.Name = Name;
-            bird.SciName = SciName;
-            bird.FamilyId = FamilyId;
-            repo.AddBird(bird);
-            return View("Admin");
+            if (model == null)
+                model = await AdminViewModel.Load();
+
+            if (Name != null && SciName != null && FamilyId != 0)
+            {
+                bird.Name = Name;
+                bird.SciName = SciName;
+                bird.FamilyId = Convert.ToInt32(FamilyId);
+
+                BirdsRepository repo = new BirdsRepository();
+                repo.AddBird(bird);
+            }
+                
+            return View("Admin", model);
         }
     }
 }
---
+++ AvesTest2/Models/AdminViewModel.cs
@@ -0,0 +1,23 @@
+﻿using AvesTest2.Database.DTO;
+using AvesTest2.Database.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvesTest2.Models
+{
+    public class AdminViewModel
+    {
+        public List<FamilyDTO> Families;
+
+        public static async Task<AdminViewModel> Load()
+        {
+            AdminViewModel model = new AdminViewModel();
+            BirdsRepository repo = new BirdsRepository();
+            model.Families = repo.Families.ToList();
+
+            return model;
+        }
+    }
+}
