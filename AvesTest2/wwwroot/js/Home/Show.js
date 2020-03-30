def597fb4e9e0d3c4986169793019ebac0951b36
Raul <rrchirinosc@gmail.com>
2020-03-30 17:49:08 +0200

    Made info display under carousel show name(sciname), location and date. Location is missing country. Changed entry width in Admin page

--- AvesTest2/wwwroot/js/Home/Show.js
+++ AvesTest2/wwwroot/js/Home/Show.js
@@ -7,7 +7,14 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
     // display bird name (sciname) stored in the alt property
-    $('#birdShow').on('slide.bs.carousel', function (event) {
-        $('#bird-name').html(this.children[0].ownerDocument.images[event.to].alt);
+    $('#birdShow').on('slid.bs.carousel', function (event) {
+        $('#bird-name').html(this.children[0].children[event.to].attributes[0].ownerElement.children[0].dataset.name);
+        $('#bird-location').html(this.children[0].children[event.to].attributes[0].ownerElement.children[0].dataset.location);
+
+        // put date into Month Day, Year format
+        let date = this.children[0].children[event.to].attributes[0].ownerElement.children[0].dataset.date.split("/");
+        let months = "January,February,March,April,May,June,July,August,September,October,Novermber,December".split(",");
+        let month = parseInt(date[1]) - 1;
+        $('#bird-date-taken').html(months[month] + " " + date[0] + ", " + date[2]);
     });
 });
