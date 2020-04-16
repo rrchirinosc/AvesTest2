7af671eb64245aa84f7e9e0703f2a197386284eb
Raul <rrchirinosc@gmail.com>
2020-04-16 10:07:15 +0200

    Improved styling of tables in the Admin page

--- AvesTest2/wwwroot/js/Admin/Admin.js
+++ AvesTest2/wwwroot/js/Admin/Admin.js
@@ -14,17 +14,21 @@
                 alert(errorThrown + this.url);
             }).done(function (birdList, textStatus, jqXHR) {
                 //alert('bird table loaded');
-                $('#bird-table').empty();
-                $('#bird-table').append(`<table>`);
+                let th = `<th style="color:#0366D6">`
+                var table = `<table><thead><tr>${th}Id</th>${th}Name</th>${th}SciName</th>${th}FamilyId</th></tr></thead><tbody>`;
+                var index = 2;
                 for (bird in birdList) {
                     let id = birdList[bird].id;
                     let name = birdList[bird].name;
                     let sciname = birdList[bird].sciName;
                     let familyid = birdList[bird].familyId;
                     let td = '<td style="padding:0 10px; color:#fff">';
-                    $('#bird-table').append(`<tr>${td}${id}</td>${td}${name}</td>${td}${sciname}</td>${td}${familyid}</td></tr>`);
+                    let tr = (index++ % 2 === 0) ? '<tr style="background-color:#444">' : '<tr>';
+                    table= table.concat(`${tr}${td}${id}</td>${td}${name}</td>${td}${sciname}</td>${td}${familyid}</td></tr>`);                   
                 }
-                $('#bird-table').append(`</table>`);
+                table = table.concat(`</tbody></table>`);
+                $('#bird-table').empty();
+                $('#bird-table').append(`${table}`);
         });
     });
 
@@ -41,16 +45,20 @@
                 alert(errorThrown + this.url);
             }).done(function (familyList, textStatus, jqXHR) {
                 //alert('family table loaded');
-                $('#family-table').empty();
-                $('#family-table').append(`<table">`);
+                let th = `<th style="color:#0366D6">`
+                var table = `<table><thead><tr>${th}Id</th>${th}Name</th>${th}SciName</th></tr></thead><tbody>`;
+                var index = 2;
                 for (family in familyList) {
                     let id = familyList[family].id;
                     let name = familyList[family].name;
                     let sciname = familyList[family].sciName;
-                    let td = '<td style="padding:0 10px; color:#fff">';
-                    $('#family-table').append(`<tr>${td}${id}</td>${td}${name}</td>${td}${sciname}</td></tr>`);
+                    let td = `<td style="padding:0 10px; color:#fff">`;
+                    let tr = (index++ % 2 === 0) ? `<tr style="background-color:#444">` : `<tr>`;
+                    table = table.concat(`${tr}${td}${id}</td>${td}${name}</td>${td}${sciname}</td></tr>`);
                 }
-                $('#family-table').append('</table>');
+                table = table.concat(`</tbody></table>`);
+                $('#family-table').empty();
+                $('#family-table').append(`${table}`);
             });
     });
 
@@ -67,8 +75,9 @@
                 alert(errorThrown + this.url);
             }).done(function (imageList, textStatus, jqXHR) {
                 //alert('image table loaded');
-                $('#image-table').empty();
-                $('#image-table').append(`<table>`);
+                let th = `<th style="color:#0366D6">`
+                var table = `<table><thead><tr>${th}BirdId</th>${th}FileName</th>${th}Location</th>${th}Date</th>${th}Country</th>${th}Coordinate</th>${th}KeyImage</th>${th}</tr></thead><tbody>`;
+                var index = 2;
                 for (image in imageList) {
                     let birdid = imageList[image].birdId;
                     let filename = imageList[image].fileName;
@@ -78,10 +87,12 @@
                     let coordinate = imageList[image].coordinate;
                     let keyimage = imageList[image].keyImage;
                     let td = '<td style="padding:0 10px; color:#fff">';
-                    $('#image-table').append(`<tr>${td}${birdid}</td>${td}${filename}</td>${td}${location}</td>` +
+                    let tr = (index++ % 2 === 0) ? '<tr style="background-color:#444">' : '<tr>';
+                    table = table.concat(`${tr}${td}${birdid}</td>${td}${filename}</td>${td}${location}</td>` +
                         `${td}${date}</td>${td}${country}</td>${td}${coordinate}</td>${td}${keyimage}</td></tr>`);
                 }
-                $('#image-table').append('</table>');
+                $('#image-table').empty();
+                $('#image-table').append(`${table}`);
             });
     });       
 });
